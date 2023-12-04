076cc264a19630c7bc42577d64d029b2bd4961a0
Rich Osman <gh@ozindfw.net>
2023-12-03 18:20:15 -0600

    Add files via upload
    Partially functional GPIO interface code.  Output doesn't seem to be called, may be .ini issue

--- drivers/Interface/PiGpioInterface.py
+++ drivers/Interface/PiGpioInterface.py
@@ -1,72 +1,175 @@
 #
 # Modified from PiFaceInterface class
 #
-#   By Rich Osman, 13 April 2023
+#   By Rich Osman, 23 November 2023
 #   Untested, and the coder is uninformed. 
 #
+#   The setup is complicated in attempt to retain compatibility with 
+#   PiFace interface code
+#
+#  2Bdo
+#   consider converting to BOARD pin numbering (as opposed to GPIO/bcm)
+#
 
-from drivers.Interface.Interface import Interface
-#import pifacedigitalio
+##################################################################################
+#
+#               NOTE
+#
+# In order to use the GPIO ports your user must be a member of the gpio group. 
+# The pi user is a member by default, other users need to be added manually.
+#  sudo usermod -a -G gpio <username>
+##################################################################################
+
+import os  # is this really needed?
+import logging  # want to add logging when more stable
+import time     # is this really needed??
+import configparser  # required to setup I/O 
 import RPi.GPIO as gpio
 
-# alternatives:
-#       RPi.GPIO Python Module  https://sourceforge.net/p/raspberry-gpio-python/wiki/Home/   https://pypi.org/project/RPi.GPIO/  https://sourceforge.net/p/raspberry-gpio-python/wiki/BasicUsage/
-#       pigpio  https://pypi.org/project/pigpio/    
-#       RPi.GPIO  https://sourceforge.net/projects/raspberry-gpio-python/
-#       
-#       gpiod  https://github.com/aswild/python-gpiod   https://wiki.loliot.net/docs/lang/python/libraries/gpiod/python-gpiod-about/  
-#       https://stackoverflow.com/questions/74352978/python-libgpiod-vs-gpiod-packages-in-linux   https://elinux.org/RPi_BCM2835_GPIOs
+import inspect 
 
+from drivers.Interface.Interface import Interface
 
 from exceptions.InvalidPositionException import InvalidPositionException
+
 import atexit
 
-
 class PiGpioInterface(Interface):
-    def setup(self):
-        # Turn off Interrupts
-        pifacedigitalio.core.deinit()
-        self.pifacedigital = pifacedigitalio.PiFaceDigital()
-        atexit.register(self.reset_piface)
-        
-        return False
-        
-    def reset_piface(self):
-        self.pifacedigital.init_board()
-        
-    def relay(self, position, value):
-        position = int(position)
 
-        if position >= 1 and position <= 2:
-            self.output(position, value)
-            return
+#
+#   PiGpioInterface.PinTranslate is used to convert .ini file IO assignments to
+#   GPIO BCM numerical values. 
+#
+#   The Limited dictionary is restricted to those GPIOs that are only
+#   GPIO functions where the FullDict dictionary contains all pins 
+#   including those that have alternate uses like serial or I2C ports.
+#
 
-        raise InvalidPositionException(
-            "Pyface has no relay position " + str(position))
+    PinTranslate = {    # Limited
+        "GPIO5" : 5, "PIN29" : 5, "GPIO6" : 6, "PIN31" : 6,
+        "GPIO16" : 16, "PIN36" : 16, "GPIO17" : 17, "PIN11" : 17,
+        "GPIO22" : 22, "PIN15" : 22, "GPIO23" : 23, "PIN16" : 23,
+        "GPIO24" : 24, "PIN18" : 24, "GPIO25" : 25, "PIN22" : 25,
+        "GPIO26" : 26, "PIN37" : 26, "GPIO27" : 27, "PIN13" : 27
+}
 
+# PinTranslate = {  # Full
+    # "GPIO5" : 5, "PIN29" : 5, "GPIO6" : 6, "PIN31" : 6,
+    # "GPIO16" : 16, "PIN36" : 16, "GPIO17" : 17, "PIN11" : 17,
+    # "GPIO22" : 22, "PIN15" : 22, "GPIO23" : 23, "PIN16" : 23,
+    # "GPIO24" : 24, "PIN18" : 24, "GPIO25" : 25, "PIN22" : 25,
+    # "GPIO26" : 26, "PIN37" : 26, "GPIO27" : 27, "PIN13" : 27,
+    # "GPIO2" : 2, "PIN3" : 2, "GPIO3" : 3, "PIN5" : 3,
+    # "GPIO4" : 4, "PIN7" : 4, "GPIO7" : 7, "PIN26" : 7,
+    # "GPIO8" : 8, "PIN24" : 8, "GPIO9" : 9, "PIN21" : 9,
+    # "GPIO10" : 10, "PIN19" : 10, "GPIO11" : 11, "PIN23" : 11,
+    # "GPIO12" : 12, "PIN32" : 12, "GPIO13" : 13, "PIN33" : 13,
+    # "GPIO14" : 14, "PIN8" : 14, "GPIO15" : 15, "PIN10" : 15,
+    # "GPIO18" : 18, "PIN12" : 18, "GPIO19" : 19, "PIN35" : 19,
+    # "GPIO20" : 20, "PIN38" : 20, "GPIO21" : 21, "PIN40" : 21
+# }
+
+#
+#   IOAssignment is used to track GPIO assignment as input or output 
+#   and prevent redundant and conflicting allocation of GPIO pins. 
+#
+#   Keys are GPIO numbers and values are: "U" for unassigned, 
+#   "I" for input, and "O" for output
+#
+
+    IOAssignment = {
+        2: "U", 3: "U", 4: "U", 5: "U", 6: "U", 7: "U", 8: "U", 
+        9: "U", 10: "U", 11: "U", 12: "U", 13: "U", 14: "U", 15: "U", 
+        16: "U", 17: "U", 18: "U", 19: "U", 20: "U", 21: "U", 22: "U", 
+        23: "U", 24: "U", 25: "U", 26: "U", 27: "U"
+    }
+    
+    def checkConfigPin(self,position,iou):
+        
+        bcm = None
+        print("\n\nBCM(None)=",bcm,"\n\n")
+
+        bcm = PiGpioInterface.PinTranslate.get(position) 
+        
+        print("\n\nBCM=",bcm,"\n\n")
+            
+        if bcm == None:
+            raise InvalidPositionException(
+                "\n\nPosiiton GPIO",position," (",BCM,") "
+                " Not available for assignment)\n\n")
+        elif PiGpioInterface.IOAssignment.get(bcm) == iou:
+            pass
+        elif PiGpioInterface.IOAssignment.get(bcm) == "U":
+            if iou == "I":
+                gpio.setup(bcm, gpio.IN, gpio.PUD_UP)
+                PiGpioInterface.IOAssignment.update({bcm : "I"})
+            elif iou == "O":
+                gpio.setup(bcm, gpio.OUT)
+                PiGpioInterface.IOAssignment.update({bcm : "O"})
+            else:
+                print ("\n\nERROR: Do not recognize IO type ",iou,
+                    " for GPIO",bcm,"\n\n")
+        else:   # already assigned something else
+            bcm = None
+            print("\n\nERROR: IO Assignment Conflict for",function,
+                " requested ",position," as ",iou,
+                " and is already set to ",PiGpioInterface.IOAssignment.get(bcm),"\n\n")   
+        return bcm
+           
+    def reset_gpio(self):
+        gpio.cleanup()
+    
+    def setup(self): 
+    
+        gpio.setmode(gpio.BCM)
+        print("\n\nIn GPIO Setup\n\n")
+        
+        frm = inspect.stack()[1]
+        mod = inspect.getmodule(frm[0])
+        print ("Called by: ",mod.__name__,"\n\n")
+        
+        return True
+       
     def input(self, position):
-        position = int(position)
+    
+        print("\n\nGPIO Input Pos:",position,"\n\n")
+        
+        frm = inspect.stack()[1]
+        mod = inspect.getmodule(frm[0])
+        print ("Called by: ",mod.__name__,"\n\n")
 
-        if position >= 1 and position <= 8:
-            return self.pifacedigital.input_pins[position-1].value
+        return gpio.input(self.checkConfigPin(position,"I"))
 
-        raise InvalidPositionException(
-            "PiFace has no input position " + str(position))
 
     def output(self, position, value):
-        position = int(position)
+    
+        print("\n\nGPIO Output Pos:",position," value: ",value,"\n\n")
+        
+        frm = inspect.stack()[1]
+        mod = inspect.getmodule(frm[0])
+        print ("Called by: ",mod.__name__,"\n\n")
+        
+        print ("Check ",self.checkConfigPin(position,"O")," Position:",position,"\n\n")
 
-        if value:
-            value = 1
-        else:
-            value = 0
+        gpio.output(self.checkConfigPin(position,"O"),value)
 
-        if position >= 1 and position <= 8:
-            if value:
-                self.pifacedigital.output_pins[position-1].turn_on()
-            else:
-                self.pifacedigital.output_pins[position-1].turn_off()
-            return
+        return True
 
-        raise InvalidPositionException(
-            "PiFace has no output position " + str(position))
+                
+#       can we just alias relay to output?
+#       relay = output (after output def)
+#    relay = output
+    
+    def relay(self, position, value):
+    
+        print("\n\nGPIO Relay Pos:",position," value: ",value,"\n\n")
+        
+        frm = inspect.stack()[1]
+        mod = inspect.getmodule(frm[0])
+        print ("Called by: ",mod.__name__,"\n\n")
+
+        gpio.output(self.checkConfigPin(position,"O"),value)
+        
+        return True
+        
+        
