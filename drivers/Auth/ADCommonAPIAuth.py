ed7f92c3f53432d74c8c1bf314e84306fc5dfcc9
Rich Osman <gh@ozindfw.net>
2023-12-07 19:15:08 -0600

    First working CommonAPI authorization
    Working CommonAPI authorization, still needs cleanup and test

--- drivers/Auth/ADCommonAPIAuth.py
+++ drivers/Auth/ADCommonAPIAuth.py
@@ -20,63 +20,65 @@ import logging
 #
 
 class ADCommonAPIAuth(Auth):
-	def __init__(self, config, loader):
-		self.mutex = threading.RLock()
-		super().__init__(config, loader)
-		
-	def setup(self):
-		self.rfid = self.getDriver('rfid')
-		self.log = self.getDriver('log')
-
-		logging.debug("Setup ADCommonAPIAuth")
-
-		self.group_allowed = self.config['group_allowed']
-		logging.debug("group_allowed: %s" % self.group_allowed)
-
-		self.processing = False
-		
-		self.rfid.observeScan(self.auth_scan)
-		self.rfid.observeScan(self.lookup_rfid)
-
-		# do not run as thread
-		return False
-
-	def auth_scan(self, id_number):
-		logging.debug("RFID scan")
-		self.notifyAuthProcessingObservers()
-	
-
-	def lookup_rfid(self, id_number):
-    
-		url = self.config['url']
-		data = '{"badge": ",'.format(id_number),'"group": "',self.config['group_allowed']}'
-		headers = {'content-type': "Content-Type: application/json"}
+    def __init__(self, config, loader):
+        self.mutex = threading.RLock()
+        super().__init__(config, loader)
         
-        print("\n\n",URL,"\n",data,"\n",headers,"\n\n")
-		response = requests.request("GET" url, data=data, headers=headers)
+    def setup(self):
+        self.rfid = self.getDriver('rfid')
+        self.log = self.getDriver('log')
 
-		json = response.json()
+        logging.debug("Setup ADCommonAPIAuth")
+
+        self.group = self.config['group']
+        logging.debug("group: %s" % self.group)
+
+        self.processing = False
+        
+        self.rfid.observeScan(self.auth_scan)
+        self.rfid.observeScan(self.lookup_rfid)
+
+        # do not run as thread
+        return False
+
+    def auth_scan(self, id_number):
+        logging.debug("RFID scan")
+        self.notifyAuthProcessingObservers()
+    
+
+    def lookup_rfid(self, id_number):
+    
+        url = self.config['url']
+        data = {"badge":id_number,"group":self.config['group']}
+        headers = {'content-type' : 'application/json'}
+        
+        response = requests.get(url, json=data, headers=headers)
+        
+        print ("\nResponse: ",response,"\n")
+
+        json = response.json()
+        
+        print ("JSON: ",json,"\n")
 #
 #       Response should be in the form:
 #       {"inGroup":true,"activeMember":true}
 #       or false as the case may be.  "activeMember" means the badge ID is found.
 #       "inGroup" means it is found in the group specified in the request.
 # 
-		if "inGroup" not in json:
-			return
+        if "inGroup" not in json:
+            return
         
         user = {
-			"authorized": json["inGroup"],
-			"id": id_number
-		}
+            "authorized": json["inGroup"],
+            "id": id_number
+        }
         
-        print ("\n\nUesr: ", user,"\n\n")
+        print ("User: ", user,"\n\n")
         
-        logging.debug(str(datetime.datetime.now()) + " ---- " + "Badge Number is ".format(id_number)," Permitted:", json["inGroup"]," Active Member: ",json(activeMember))
+#        logging.debug(str(datetime.datetime.now()) + " ---- " + "Badge Number is ".format(id_number)," Permitted:", json["inGroup"]," Active Member: ",json(activeMember))
 
-
-		self.notifyAuthObservers(user)
+        self.notifyAuthObservers(user)
 
 
 synchronize(ADCommonAPIAuth, "auth_scan, lookup_rfid")
-		
+        
