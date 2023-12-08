1ef8d29fc652d3b8c8babd3f678064cc55cc5fdd
Rich Osman <gh@ozindfw.net>
2023-12-07 21:10:37 -0600

    Initial untested whack

---
+++ drivers/Log/SysLogger.py
@@ -0,0 +1,59 @@
+#
+# Modified from FileLog class
+#
+#   By Rich Osman, 7 December 2023
+#   Untested, and the coder is uninformed. 
+#
+#   Editing from the original is not complete. 
+#
+
+from drivers.Log.Log import Log
+import logging
+import logging.handlers
+import syslog
+
+class SysLogger(Log):
+    def __init__(self, config, loader):
+        super().__init__(config, loader)
+
+        logmask = {"error":LOG_ERR, "info":LOG_INFO, "debug":LOG_DEBUG}
+
+        format = "%(asctime)-15s %(message)s"
+        datefmt = '%Y-%m-%d %H:%M:%S'
+        loglevel = logging.LOG_DEBUG
+        
+        logger = logging.getLogger()
+        logger.setLevel(logging.DEBUG)
+
+        syslog = logging.handlers.SysLogHandler(address='/dev/log')
+        formatter = logging.Formatter('%(module)s: %(message)s')
+        syslog.setFormatter(formatter)
+        logger.add
+        
+        if 'log_level' in config:
+            syslog.setlogmask(logmask{config['log_level'].lower()})
+
+        Handler(syslog)
+#        logger.info('This is an info message')
+
+        if 'format' in config: format = config['format']
+
+        
+        if 'date_format' in config: datefmt = config['date_format']
+
+    def auth(self, user):
+        logger.info("Auth: " + str(user))
+
+    def engaged(self, status):
+        logger.info("Engaged: " + str(status))
+
+    def debug(self, message):
+        logger.debug(message)
+
+    def info(self, message):
+        logger.info(message)
+    
+    def error(self, message):
+        logger.error(message)
+    
+
