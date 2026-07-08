19890fb5ebe09374173632df6e85c5bcfd6b3afa
Rich Osman <gh@ozindfw.net>
2026-07-08 16:08:32 -0500

    Added switch debounce

--- drivers/CabinetSense/CabinetSwitchSense.py
+++ drivers/CabinetSense/CabinetSwitchSense.py
@@ -1,3 +1,11 @@
+#
+#	This module reads and processes door and latch sense switches into
+#	a state change notification
+#
+#	Rich Osman OZINDFW 4 July 2026
+#
+
+
 from drivers.CabinetSense.CabinetSense import CabinetSense
 import time
 import logging
@@ -7,7 +15,7 @@ class CabinetSwitchSense(CabinetSense):
 	def setup(self):
 		self.interface = self.getDriver('cabinet_sense_interface')
 		self.log = self.getDriver('log')
-		old_state = "closed"
+		state = "closed"
 		return True
 		
 #	Switch State 0 = closed; 1 = open
@@ -33,17 +41,35 @@ class CabinetSwitchSense(CabinetSense):
 		return state
 
 	def run(self):
-		old_state = self.getValue()
+		state = self.getValue()
+		
+		debounce_tries = 4
+		if 'debounce_tries' in self.config:
+			debounce_tries = self.config['debounce_tries']
 
 		try:
 			while(True):
-				time.sleep(0.1)
-				new_state = self.getValue()
-				if old_state != new_state:
-					logging.debug(f"Switch state change to: {new_state}")
-					old_state = new_state
-					self.notifyCabinetChangeObservers(new_state)
-					
+				
+#
+#	Door switches in some installations need debouncing because the 
+#	doors themselves flex when hit and cause effective bouncing of the switches.
+#	the switches.
+#
+				tries = 1
+				for loop in range (0,debounce_tries):
+					time.sleep(0.1)
+					if state != self.getValue():
+#						check = self.getValue()
+#						logging.debug(f"Switch check state: {check}")
+#
+#						logging.debug(f"Switch state debounce try: {tries}")
+						tries += 1
+					else: break
+				else:
+					state = self.getValue()
+					logging.debug(f"Switch state change to: {state}")
+					self.notifyCabinetChangeObservers(state)
+				
 		except Exception as e:
 			logging.error("Exception: %s" % str(e), exc_info=1)
 			os._exit(42) # Make sure entire application exits
